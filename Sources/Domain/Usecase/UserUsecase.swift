b590ec51c7b1ec7a82d65f1e7b9fc215ba7109e1
partnertientm2 <partnertientm2@abbank.vn>
2025-12-18 17:19:58 +0700

    refactor

--- Sources/Domain/Usecase/UserUsecase.swift
+++ Sources/Domain/Usecase/UserUsecase.swift
@@ -10,7 +10,7 @@ import Combine
 
 public protocol UserUsecase {
     
-    func getListUser(perPage: Int, since: Int) -> AnyPublisher<Result<User, APIError>, Never>
+    func getListUser(perPage: Int, since: Int) -> AnyPublisher<Result<[User], APIError>, Never>
     func getUserDetails(userName: String) -> AnyPublisher<Result<UserDetails, APIError>, Never>
 }
 
@@ -22,7 +22,7 @@ public class UserUsecaseImpl: UserUsecase {
         self.repository = repository
     }
     
-    public func getListUser(perPage: Int, since: Int) -> AnyPublisher<Result<User, APIError>, Never> {
+    public func getListUser(perPage: Int, since: Int) -> AnyPublisher<Result<[User], APIError>, Never> {
         repository.getListUser(perPage: perPage, since: since)
             .autoDecode()
     }
